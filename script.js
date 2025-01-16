c58e591525286e3b736691c9d4ba656f202b7b02
sajdeen <sajdeen0786@gmail.com>
2025-01-17 01:18:48 +0530

    done

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
-let string = "";
-let buttons = document.querySelectorAll('.button');
-Array.from (buttons).forEach ((button)=>{
-    button.addEventListener('click', (e)=>{
-        consol.log (e.target)
+const resulprevitDisplay = document.getElementById('result');
+const buttons = document.querySelectorAll('.button');
+
+let currentInput = '';
+let previousInput = '';
+let operator = '';
+
+buttons.forEach(button =>{
+    button.addEventListener('click', () =>{
+        const value = button.textContent;
+
+        //clear all
+
+        if(value ==='AC'){
+            currentInput =
+            
+        }
     })
 })
