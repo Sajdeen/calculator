4704f8bdcded112030653d8f7f896fc0a6afd353
sajdeen <sajdeen0786@gmail.com>
2025-06-06 00:00:23 +0530

    done

--- script.js
+++ script.js
@@ -1,25 +1,20 @@
-const resultDisplay = document.getElementById('result');
-const buttons = document.querySelectorAll('.button');
-
-let currentInput = '';
-let previousOperand = '';
-let operation = null; 
-
-
-//Add event listeners to buttons
+let string ="";
+let buttons = document.querySelectorAll(".button");
 
 buttons.forEach((button) => {
-    button.addEventListener('click', () => {
-        const value = button.innerText;
+    button.addEventListener("click", (e) => {
+        if (e.target.innerHTML === "=") {
+          string = eval(string);
+          document.querySelector("input").value = string;
+        } 
+        console.log(e.target)
+        string = string + e.target.innerHTML;
+        document.querySelector('input').value = string;
+      }) 
+}) else if (value==="AC") {
+  string = "";
+  document.querySelector('input').value ="";
+} else if (value === "DEL") {
+  string
+}
 
-        //clear all
-
-        if (value === 'AC') {
-            currentInput = '';
-            previousOperand = '';
-            operation = null;
-            resultDisplay, value = '';
-
-        }
-    });
-});  
